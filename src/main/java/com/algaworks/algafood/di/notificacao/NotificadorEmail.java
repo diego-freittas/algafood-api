ba198ccef0ad3ac6965b1ec61813c449d68ebf8d
diego-freittas <tpddiego@gmail.com>
2021-08-09 16:25:13 -0300

    Acessando propriedades com @ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.modelo.Cliente;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
@@ -19,10 +20,14 @@ public class NotificadorEmail implements Notificador {
     @Getter
     private String hostSevidorSmtp;
 
-    @Value("${notificador.email.host-servidor}") // fazendo uma injerção de valor
-    private String host;
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    private NotificadorProperties notificadorProperties;
+
+    // Foi criado uma classe NotificadorProperties para não precisar passar as propriedades dessa forma.
+//    @Value("${notificador.email.host-servidor}") // fazendo uma injerção de valor
+ //    private String host;
+//    @Value("${notificador.email.porta-servidor}")
+     //private Integer porta;
 
 
 /*    public NotificadorEmail (String hostSevidorSmtp){
@@ -36,8 +41,10 @@ public class NotificadorEmail implements Notificador {
         }
         System.out.printf("Notificando %s através do e-mail %s usando o SMTP %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), this.getHostSevidorSmtp(), mensagem);
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+//        System.out.println("Host: " + host);
+        System.out.println("Host: " + notificadorProperties.getHostServidor());
+//        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + notificadorProperties.getPortaServidor());
     }
 
 
---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorProperties.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.di.notificacao;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("notificador.email")
+@Getter
+@Setter
+public class NotificadorProperties {
+
+    private String hostServidor;
+    private Integer portaServidor = 25;
+
+}
