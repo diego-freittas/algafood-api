de22a70bdd8f42db7d9e06a2227e9312318a38f9
diego-freittas <tpddiego@gmail.com>
2021-09-11 13:16:37 -0300

    Atualiza o Spring da versão 2.1.0 para 2.5.0.
    Esse commit atualiza o Spring para ficar compatível com a versão do JUnit 5.

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,17 +1,19 @@
 package com.algaworks.algafood;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class AlgafoodApiApplicationTests {
+public class CadastroCozinhaIntegrationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads(){
+        Assertions.assertFalse(true);
+    }
 
 }
