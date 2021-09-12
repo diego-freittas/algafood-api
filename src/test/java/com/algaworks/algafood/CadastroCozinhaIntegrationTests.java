35b8458b32006b8be0a11271c07930c706c6677f
diego-freittas <tpddiego@gmail.com>
2021-09-12 14:55:44 -0300

    Configura o JUnit 5

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,9 +1,13 @@
 package com.algaworks.algafood;
 
 
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CozinhaService;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -11,9 +15,34 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 public class CadastroCozinhaIntegrationTests {
 
+    @Autowired
+    private CozinhaService cozinhaService;
+
     @Test
-    public void contextLoads(){
-        Assertions.assertFalse(true);
+    public void testarCadastroCozinhaComSucesso(){
+        //Teste do caminho feliz
+
+        //Cenário
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome("Chinesa do teste");
+        //Ação
+        novaCozinha = cozinhaService.salvar(novaCozinha);
+        //Validação
+        org.assertj.core.api.Assertions.assertThat(novaCozinha).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(novaCozinha.getId()).isNotNull();
+    }
+
+    @Test
+    public void deveFalharAoCadastrarCozinha_QuandoSemNome(){
+        //Cenário
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome(null);
+        //Ação
+        //Validação
+        ConstraintViolationException erroEsperado =
+                Assertions.assertThrows(ConstraintViolationException.class, () -> {
+                    cozinhaService.salvar(novaCozinha);
+                });
     }
 
 }
